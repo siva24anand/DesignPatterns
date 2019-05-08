b9831155f63d07b80af8761983bef37b1872bcc9
Sivanandham <siva24anand@gmail.com>
2019-05-08 23:05:26 +0530

    observer changes

---
+++ DesignPatternConsole/DesignPatternConsole/Observer/Investor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternConsole.Observer
+{
+    public class Investor: IInvestor
+    {
+        public void Update(Stock stock)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatternConsole/DesignPatternConsole/Observer/Stock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternConsole.Observer
+{
+    public abstract class Stock
+    {
+        private string _symbol;
+        private double _price;
+        private List<IInvestor> _investors = new List<IInvestor>();
+
+        public Stock(string symbol, double price)
+        {
+            _symbol = symbol;
+            _price = price;
+        }
+
+        public void Attach(IInvestor investor)
+        {
+            _investors.Add(investor);
+        }
+        public void Detach(IInvestor investor)
+        {
+            _investors.Remove(investor);
+        }
+        public void Notify()
+        {
+            foreach (IInvestor investor in _investors)
+            {
+                
+            }
+        }
+        public string Symbol
+        {
+            get
+            {
+                return _symbol;
+            }
+        }
+    }
+}
